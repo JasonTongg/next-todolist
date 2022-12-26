f051a0506e960e58028038e08ee825c83e8928d3
JasonTongg <aboytong123@gmail.com>
2022-12-26 19:16:24 +0700

    add title and meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <title>Next - Todolist</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
